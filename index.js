af322d896c6f1570ab78ddbee20cd47d0dc10c93
seanwsutter <seanwsutter@gmail.com>
2023-11-01 17:21:37 -0500

    Added questions, generateMarkdown.js, basic functionality

--- index.js
+++ index.js
@@ -1,13 +1,136 @@
+// packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user input
+const questions = [
+  {
+    name: "title",
+    type: "input",
+    message: "What is the project title?"
+  },
+  {
+    name: "description",
+    type: "input",
+    message: "Can you give a description of the project"
+  },
+  {
+    name: "installation",
+    type: "input",
+    message: "What are the installation instructions?"
+  },
+  {
+    name: "usage",
+    type: "input",
+    message: "What is the usage information?"
+  },
+
+  // license
+  {
+    name: "license",
+    type: "list",
+    message: "Select the appropiate license for this project.",
+    choices: [
+      "Apache License 2.0",
+      "Boost Software License 1.0",
+      "BSD 3-Clause License",
+      "BSD 2-Clause License"
+    ],
+  },
+  {
+    name: "contributions",
+    type: "input",
+    message: "What are the contribution guidelines for this project?"
+  },
+  {
+    name: "testing",
+    type: "input",
+    message: "What are the instructions for testing?"
+  },
+  // contaact
+  {
+    name: "username",
+    type: "input",
+    message: "What is your GitHub username?"
+  },
+  {
+    name: "email",
+    type: "input",
+    message: "What is your email address?"
+  },
+
+];
+
+
+// function to write README file
+function writeToFile(fileName, response) {
+  fs.appendFile(fileName, response, (error) => error ?
+
+    console.error(error) : console.log("writeToFile function sucess"));
+}
+
+// function to initialize app
+function init() {
+  inquirer.prompt(questions).then(response => {
+    console.log("response:", response);
+
+    writeToFile("./example/README.md", generateMarkdown(response));
+    console.log("generate README success");
+  })
+}
+
+
+init();
+
+
+
+
+
+
+/* comments
+
+
+// function to write README file
+function writeToFile(fileName, data) {
+  fs.appendFile(fileName, data, (error) =>
+    error ? console.log(error) :
+      console.log("wirteToFile was a sucess"));
+}
+
+// function to initialize app
+function init() {
+  inquirer.prompt(questions).then(data => {
+      console.log("data from questions:", data);
+    })
+  writeToFile("./sample/README.md", generateMarkdown(data))
+};
+
+fs.appendFile(fileName, data, (error) => {
+  error ? console.log(error) : console.log("wirteToFile sucess");
+})
+}
+
+fs.appendFile(fileName, data)
+if (error) {
+    console.log("error:", error);
+}
+
+
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
+
+
+
+*/
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,72 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function that will return license reponse with BADGE + URL
+function renderLicenseBadge(response) {
+  var licenseOption = response.license;
+  var licenseChoice;
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  if (licenseOption = "Apache License 2.0") {
+    licenseChoice = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  } else {
+    licenseChoice = "";
+  }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  return licenseChoice;
+}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// function to generate README
+function generateMarkdown(response) {
+  return `# ${response.title}  
+  ${renderLicenseBadge(response)}
+  ## Description
+  ${response.description}
+  
+  ## Table of Contents
+  - [Description](#description)
+  - Installation
+  - Usage
+  - License
+  - Contributions
+  - Testing
+  - Questions
+
+  ## Installation
+  ${response.installation}
+
+  ## Usage
+  ${response.usage}
+
+  ## License
+  ${response.license} 
+
+  ## Contributions
+  ${response.contributions}
+
+  ## Testing
+  ${response.testing}
+
+  ## Questions & Contact Info
+  [GitHub Account Page URL](https://github.com/${response.username})
+
+  Email: ${response.email}
 
 `;
 }
 
 module.exports = generateMarkdown;
+
+
+
+/* comments
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+// function renderLicenseLink(license) { }
+
+
+  [URL](${renderLicenseSection(response)})
+
+// function that returns a license badge icon
+function renderLicenseBadge(response) {}
+
+
+
+*/
